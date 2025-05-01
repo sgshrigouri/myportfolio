106306205abcdd382f75c7aa100da05de42dd0f9
sgshrigouri <sgshrigouri@gmail.com>
2025-05-01 11:39:53 +0530

    updated issues regarding popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,113 +1,114 @@
 /*
-	Strata by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    Strata by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var $window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$footer = $('#footer'),
-		$main = $('#main'),
-		settings = {
-			// Parallax background effect?
-				parallax: true,
+    var $window = $(window),
+        $body = $('body'),
+        $header = $('#header'),
+        $footer = $('#footer'),
+        $main = $('#main'),
+        settings = {
+            // Parallax background effect?
+            parallax: true,
 
-			// Parallax factor (lower = more intense, higher = less intense).
-				parallaxFactor: 20
-		};
+            // Parallax factor (lower = more intense, higher = less intense).
+            parallaxFactor: 20
+        };
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1800px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '481px',   '736px'  ],
-			xsmall:  [ null,      '480px'  ],
-		});
+    // Breakpoints.
+    breakpoints({
+        xlarge:  [ '1281px',  '1800px' ],
+        large:   [ '981px',   '1280px' ],
+        medium:  [ '737px',   '980px'  ],
+        small:   [ '481px',   '736px'  ],
+        xsmall:  [ null,      '480px'  ],
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Touch?
-		if (browser.mobile) {
-			// Turn on touch mode.
-				$body.addClass('is-touch');
+    // Touch?
+    if (browser.mobile) {
+        // Turn on touch mode.
+        $body.addClass('is-touch');
 
-			// Height fix (mostly for iOS).
-				window.setTimeout(function() {
-					$window.scrollTop($window.scrollTop() + 1);
-				}, 0);
-		}
+        // Height fix (mostly for iOS).
+        window.setTimeout(function() {
+            $window.scrollTop($window.scrollTop() + 1);
+        }, 0);
+    }
 
-	// Footer.
-		breakpoints.on('<=medium', function() {
-			$footer.insertAfter($main);
-		});
+    // Footer.
+    breakpoints.on('<=medium', function() {
+        $footer.insertAfter($main);
+    });
 
-		breakpoints.on('>medium', function() {
-			$footer.appendTo($header);
-		});
+    breakpoints.on('>medium', function() {
+        $footer.appendTo($header);
+    });
 
-	// Header.
-		// Parallax background.
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (browser.name == 'ie' || browser.mobile)
-					settings.parallax = false;
+    // Header.
+    // Parallax background.
+    // Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+    if (browser.name === 'ie' || browser.mobile) {
+        settings.parallax = false;
+    }
 
-			if (settings.parallax) {
-				breakpoints.on('<=medium', function() {
-					$window.off('scroll.strata_parallax');
-					$header.css('background-position', '');
-				});
+    if (settings.parallax) {
+        breakpoints.on('<=medium', function() {
+            $window.off('scroll.strata_parallax');
+            $header.css('background-position', '');
+        });
 
-				breakpoints.on('>medium', function() {
-					$header.css('background-position', 'left 0px');
-					$window.on('scroll.strata_parallax', function() {
-						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-					});
-				});
+        breakpoints.on('>medium', function() {
+            $header.css('background-position', 'left 0px');
+            $window.on('scroll.strata_parallax', function() {
+                $header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+            });
+        });
 
-				$window.on('load', function() {
-					$window.triggerHandler('scroll');
-				});
-			}
+        $window.on('load', function() {
+            $window.triggerHandler('scroll');
+        });
+    }
 
-	// Main Sections: Two.
-		// Lightbox gallery.
-		$(window).on('load', function() {
-			$('#two').poptrox({
-				caption: function($a) { return $a.next('h3').text(); },
-				overlayColor: '#2c2c2c',
-				overlayOpacity: 0.85,
-				popupCloserText: '',
-				popupLoaderText: '',
-				selector: '.work-item a.image',
-				usePopupCaption: true,
-				usePopupDefaultStyling: false,
-				usePopupEasyClose: false,
-				usePopupNav: true,
-				windowMargin: (breakpoints.active('<=small') ? 0 : 50),
-				onPopupOpen: function() {
-					// Log the href to debug
-					console.log('Poptrox onPopupOpen - href:', this.element.attr('href'));
-					// Use absolute path for GitHub Pages
-					var href = this.element.attr('href');
-					if (href && !href.startsWith('/myportfolio/')) {
-						href = '/myportfolio/' + href;
-					}
-					// Append button with custom class
-					$('.poptrox-popup').append(
-						'<a href="' + href + '" class="button go-to-project">Go to Project</a>'
-					);
-				}
-			});
-		});
+    // Main Sections: Two.
+    // Lightbox gallery.
+    $(window).on('load', function() {
+        $('#two').poptrox({
+            caption: function($a) { return $a.next('h3').text(); },
+            overlayColor: '#2c2c2c',
+            overlayOpacity: 0.85,
+            popupCloserText: '',
+            popupLoaderText: '',
+            selector: '.work-item a.image',
+            usePopupCaption: true,
+            usePopupDefaultStyling: false,
+            usePopupEasyClose: false,
+            usePopupNav: true,
+            windowMargin: (breakpoints.active('<=small') ? 0 : 50),
+            onPopupOpen: function() {
+                // Log the href to debug
+                console.log('Poptrox onPopupOpen - href:', this.element.attr('href'));
+                // Use absolute path for GitHub Pages
+                var href = this.element.attr('href');
+                if (href && !href.startsWith('/myportfolio/')) {
+                    href = '/myportfolio/' + href;
+                }
+                // Append button with custom class
+                $('.poptrox-popup').append(
+                    '<a href="' + href + '" class="button go-to-project">Go to Project</a>'
+                );
+            }
+        });
+    });
 
 })(jQuery);
